0355ffdbc5b281d716b1f212f7f18de05bd7ff7e
ST9527 <st9527.cs229@gmail.com>
2016-01-11 12:00:00 +0800

    添加uwsgi配置

--- django_game_server/wsgi.py
+++ django_game_server/wsgi.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 """
 WSGI config for django_game_server project.
 
@@ -9,8 +10,12 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 import manage
-
+from uwsgidecorators import timer
 from django.core.wsgi import get_wsgi_application
+from django.utils import autoreload
+# The uWSGI server automagically adds a uwsgi module into your Python apps.
+# http://uwsgi-docs.readthedocs.org/en/latest/PythonModule.html
+import uwsgi
 
 # check whether the log dir is exits
 manage.check_log()
@@ -18,3 +23,10 @@ manage.check_log()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_game_server.settings")
 
 application = get_wsgi_application()
+
+
+# 发现代码发生变化自动热重启
+@timer(3)
+def change_code_gracefully_reload(sig):
+    if autoreload.code_changed():
+        uwsgi.reload()
